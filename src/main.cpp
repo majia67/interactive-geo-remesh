99e014af50d62e9755935efca1daf07d336831cb
Yicong Tao <yt1182@nyu.edu>
2018-04-25 16:29:03 -0400

    adding option to do harmonic parameterization

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/readOFF.h>
 #include <igl/opengl/glfw/imgui/ImGuiMenu.h>
+#include <igl/boundary_loop.h>
+#include <igl/harmonic.h>
+#include <igl/map_vertices_to_circle.h>
 #include <Eigen/Core>
 
-//vertex array, #V x3
-Eigen::MatrixXd V(0, 3);
-//face array, #F x3
-Eigen::MatrixXi F(0, 3);
+Eigen::MatrixXd V(0, 3);                //vertex array, #V x3
+Eigen::MatrixXi F(0, 3);                //face array, #F x3
+Eigen::MatrixXd V_uv(0, 2);             //vertex array in the UV plane, #V x3
+
+void harmonic_parameterization();
 
 int main(int argc, char *argv[])
 {
@@ -24,12 +28,45 @@ int main(int argc, char *argv[])
 
     // Plot the mesh
     igl::opengl::glfw::Viewer viewer;
-
-    igl::opengl::glfw::imgui::ImGuiMenu menu;
-    viewer.plugins.push_back(&menu);
-
     viewer.data().clear();
     viewer.data().set_mesh(V, F);
 
+    // Setup the menu
+    igl::opengl::glfw::imgui::ImGuiMenu menu;
+    viewer.plugins.push_back(&menu);
+    // Add content to the default menu window
+    menu.callback_draw_viewer_menu = [&]()
+    {
+        // Draw parent menu content
+        menu.draw_viewer_menu();
+
+        // Add new group
+        if (ImGui::CollapsingHeader("Remeshing Controls", ImGuiTreeNodeFlags_DefaultOpen))
+        {
+            if (ImGui::Button("Parameterize"))
+            {
+                harmonic_parameterization();
+
+                viewer.data().set_mesh(V_uv, F);
+                viewer.data().set_uv(V_uv);
+                viewer.core.align_camera_center(V_uv, F);
+            }
+        }
+    };
+    
     viewer.launch();
+}
+
+void harmonic_parameterization()
+{
+    // Find the open boundary
+    Eigen::VectorXi bnd;
+    igl::boundary_loop(F, bnd);
+
+    // Map the boundary to a circle, preserving edge proportions
+    Eigen::MatrixXd bnd_uv;
+    igl::map_vertices_to_circle(V, bnd, bnd_uv);
+
+    // Harmonic parametrization for the internal vertices
+    igl::harmonic(V, F, bnd, bnd_uv, 1, V_uv);
 }
