3443c1a10e9f165bba3c1237305a3e1a5f9db26c
Yicong Tao <yt1182@nyu.edu>
2018-04-25 16:06:02 -0400

    mesh viewing & show default menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,35 @@
-int main() {
-    return 0;
+#include <iostream>
+
+#include <igl/opengl/glfw/Viewer.h>
+#include <igl/readOFF.h>
+#include <igl/opengl/glfw/imgui/ImGuiMenu.h>
+#include <Eigen/Core>
+
+//vertex array, #V x3
+Eigen::MatrixXd V(0, 3);
+//face array, #F x3
+Eigen::MatrixXi F(0, 3);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: remesh_bin mesh.off" << std::endl;
+        exit(0);
+    }
+
+    // Read mesh
+    igl::readOFF(argv[1], V, F);
+    assert(V.rows() > 0);
+
+    // Plot the mesh
+    igl::opengl::glfw::Viewer viewer;
+
+    igl::opengl::glfw::imgui::ImGuiMenu menu;
+    viewer.plugins.push_back(&menu);
+
+    viewer.data().clear();
+    viewer.data().set_mesh(V, F);
+
+    viewer.launch();
 }
